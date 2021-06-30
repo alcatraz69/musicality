6398568e749cf4cabe6d4ed20d3e16bc4b3d1e60
Prem Kantikar <48849830+alcatraz69@users.noreply.github.com>
2021-06-30 16:47:32 +0530

    feat - added user profile page

--- src/components/RightBar/Rightbar.js
+++ src/components/RightBar/Rightbar.js
@@ -1,41 +1,124 @@
 import "./Rightbar.css";
 import { BsThreeDots } from "react-icons/bs";
-const Rightbar = () => {
+
+const Rightbar = ({ profile }) => {
+  const HomeRightbar = () => {
+    return (
+      <>
+        <div className="birthdayContainer">
+          <img src="/Asset/gift.png" alt="" className="bdayImg" />
+          <span className="bdayText">
+            <b>James Conner</b> and <b>2 others</b> have birthdays today
+          </span>
+        </div>
+
+        <a
+          href="https://musicflix.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+          className="sponserContainer"
+        >
+          <img src="/Asset/mad.png" alt="" className="sponserImg" />
+          <div className="sponserTxtContainer">
+            <div className="sponserTxt">Best Music Playlists</div>
+            <div className="sponserLink">musicflix.com</div>
+          </div>
+        </a>
+
+        <div className="friendlistTitleContainer">
+          <h4 className="friendsTitle">Friends</h4>
+          <BsThreeDots style={{ marginRight: "50px" }} />
+        </div>
+
+        <ul className="friendList">
+          <li className="friendItem">
+            <div className="friendProfileImgContainer">
+              <img src="/Asset/user/8.jpeg" alt="" className="postProfileImg" />
+              <span className="onlineStatus"></span>
+            </div>
+            <span className="friendUserName">Zac Efron</span>
+          </li>
+        </ul>
+      </>
+    );
+  };
+
+  const ProfileRightbar = () => {
+    return (
+      <>
+        <h4 className="rightbarTitle">User information</h4>
+        <div className="rightbarInfo">
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">City:</span>
+            <span className="rightbarInfoValue">New York</span>
+          </div>
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">From:</span>
+            <span className="rightbarInfoValue">Madrid</span>
+          </div>
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">Relationship:</span>
+            <span className="rightbarInfoValue">Single</span>
+          </div>
+        </div>
+        <h4 className="rightbarTitle">User friends</h4>
+        <div className="rightbarFollowings">
+          <div className="rightbarFollowing">
+            <img
+              src="/Asset/user/1.jpeg"
+              alt=""
+              className="rightbarFollowingImg"
+            />
+            <span className="rightbarFollowingName">John Carter</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img
+              src="/Asset/user/2.jpeg"
+              alt=""
+              className="rightbarFollowingImg"
+            />
+            <span className="rightbarFollowingName">John Carter</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img
+              src="/Asset/user/3.jpeg"
+              alt=""
+              className="rightbarFollowingImg"
+            />
+            <span className="rightbarFollowingName">John Carter</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img
+              src="/Asset/user/4.jpeg"
+              alt=""
+              className="rightbarFollowingImg"
+            />
+            <span className="rightbarFollowingName">John Carter</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img
+              src="/Asset/user/7.jpeg"
+              alt=""
+              className="rightbarFollowingImg"
+            />
+            <span className="rightbarFollowingName">John Carter</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img
+              src="/Asset/user/9.jpeg"
+              alt=""
+              className="rightbarFollowingImg"
+            />
+            <span className="rightbarFollowingName">John Carter</span>
+          </div>
+        </div>
+      </>
+    );
+  };
+
   return (
     <div className="rightbar">
-      <div className="birthdayContainer">
-        <img src="/Asset/gift.png" alt="" className="bdayImg" />
-        <span className="bdayText">
-          <b>James Conner</b> and <b>2 others</b> have birthdays today
-        </span>
-      </div>
-
-      <a
-        href="https://musicflix.netlify.app/"
-        target="_blank"
-        rel="noreferrer"
-        className="sponserContainer"
-      >
-        <img src="/Asset/mad.png" alt="" className="sponserImg" />
-        <div className="sponserTxtContainer">
-          <div className="sponserTxt">Best Music Playlists</div>
-          <div className="sponserLink">musicflix.com</div>
-        </div>
-      </a>
-      <div className="friendlistTitleContainer">
-        <h4 className="friendsTitle">Friends</h4>
-        <BsThreeDots style={{ marginRight: "50px" }} />
-      </div>
-
-      <ul className="friendList">
-        <li className="friendItem">
-          <div className="friendProfileImgContainer">
-            <img src="/Asset/user/8.jpeg" alt="" className="postProfileImg" />
-            <span className="onlineStatus"></span>
-          </div>
-          <span className="friendUserName">Zac Efron</span>
-        </li>
-      </ul>
+      {profile ? <ProfileRightbar /> : <HomeRightbar />}
     </div>
   );
 };
--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,7 +1,34 @@
-import "/Profile.css";
+import "./Profile.css";
+import Navbar from "../../components/NavBar/Navbar";
+import Leftbar from "../../components/LeftBar/Leftbar";
+import Feed from "../../components/Feed/Feed";
+import Rightbar from "../../components/RightBar/Rightbar";
 
 const Profile = () => {
-  return <div></div>;
+  return (
+    <>
+      <Navbar />
+      <div className="profileContainer">
+        <Leftbar />
+        <div className="profileRight">
+          <div className="profileRightTop">
+            <div className="profileCover">
+              <img src="/Asset/posts/3.jpeg" alt="" className="coverPic" />
+              <img src="/Asset/user/1.jpeg" alt="" className="profilePic" />
+            </div>
+            <div className="profileInfo">
+              <h4 className="profileInfoName">Alexandra Borke</h4>
+              <span className="profileInfoDesc">Hello Friends! </span>
+            </div>
+          </div>
+          <div className="profileRightBottom">
+            <Feed />
+            <Rightbar profile />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Profile;
