def30b3aad7dd746374c1af3cf1ce1618cb2d3de
Prem Kantikar <48849830+alcatraz69@users.noreply.github.com>
2021-07-17 10:12:01 +0530

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  // baseURL: "https://musicality-backend.herokuapp.com",
-  baseURL: "http://localhost:5000",
+   baseURL: "https://musicality-backend.herokuapp.com",
+  //baseURL: "http://localhost:5000",
 });
 
 const config = () => {
